dbe7b22df1765efd0ff3c6e3374807f2089a2fb5
GitHub Action <action@github.com>
2022-06-21 19:22:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/paulpanther/intellijsmalltalk/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/paulpanther/intellijsmalltalk/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.paulpanther.intellijsmalltalk.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.paulpanther.intellijsmalltalk.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/paulpanther/intellijsmalltalk/services/MyProjectService.kt
+++ src/main/kotlin/com/github/paulpanther/intellijsmalltalk/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.paulpanther.intellijsmalltalk.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.paulpanther.intellijsmalltalk.MyBundle
 
 class MyProjectService(project: Project) {
 
