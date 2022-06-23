281f6890af6c5387bcc1fe314b7ceb85cc1c8440
Paul Methfessel <paul.methfessel@web.de>
2022-06-24 00:33:55 +0200

    Fix RunConfiguration for Unix

--- src/main/kotlin/com/github/paulpanther/intellijsmalltalk/editor/run/SmalltalkRunConfigurationOptions.kt
+++ src/main/kotlin/com/github/paulpanther/intellijsmalltalk/editor/run/SmalltalkRunConfigurationOptions.kt
@@ -3,18 +3,7 @@ package com.github.paulpanther.intellijsmalltalk.editor.run
 import com.intellij.execution.configurations.RunConfigurationOptions
 
 class SmalltalkRunConfigurationOptions: RunConfigurationOptions() {
-    private val myScriptName = string("").provideDelegate(this, "scriptName")
-    private val myImageName = string("").provideDelegate(this, "imageName")
-
-    var scriptName: String?
-        get() = myScriptName.getValue(this)
-        set(value) {
-            myScriptName.setValue(this, value)
-        }
-
-    var imageName: String?
-        get() = myImageName.getValue(this)
-        set(value) {
-            myImageName.setValue(this, value)
-        }
+    var squeakPath: String? by string("").provideDelegate(this, this::squeakPath)
+    var scriptPath: String? by string("").provideDelegate(this, this::scriptPath)
+    var imagePath: String? by string("").provideDelegate(this, this::imagePath)
 }
